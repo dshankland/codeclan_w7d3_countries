07892faf610d26f2ccb81249596420fc412825ef
Darren Shankland <ds@4pq.co.uk>
2019-05-01 19:36:54 +0100

    hacking about with select

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>Countries</h1>
     <div class="main-container">
+      <countries-dropdown v-bind:countries='countries'></countries-dropdown>
       <countries-list v-bind:countries='countries'></countries-list>
       <country-detail :country='selectedCountry'></country-detail>
     </div>
@@ -10,6 +11,7 @@
 
 <script>
 import CountriesList from './components/CountriesList.vue';
+import CountriesDropdown from './components/CountriesDropdown.vue';
 import CountryDetail from './components/CountryDetail.vue';
 import { eventBus } from './main.js'
 
@@ -37,14 +39,15 @@ export default {
   mounted(){
     // console.log('loaded');
     this.fetchCountries();
-    console.log(this.countries);
+    // console.log(this.countries);
     eventBus.$on('country-selected', (country) => {
-      // console.log('within $on', munro);
+      // console.log('within $on', country);
       this.selectedCountry = country;
     });
 },
   components: {
     'countries-list': CountriesList,
+    'countries-dropdown': CountriesDropdown,
     'country-detail': CountryDetail
   }
 }
---
+++ src/components/CountriesDropdown.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+  <div>
+    <p>The Countries Dropdown</p>
+    <select v-on:change="handleChange" v-model="selected">
+      <option selected>Select Country</option>
+      <!-- v-model in the select is the relevant part, v-bind:value would be for a form -->
+      <option v-for="(country, index) in countries">{{ country.name }}</option>
+    </select>
+      <!-- <countries-list-item v-for="(country, index) in countries" :country="country" :key="index">{{country.name}}</countries-list-item> -->
+  </div>
+
+</template>
+
+<script>
+import { eventBus } from '../main.js'
+
+export default {
+  name: 'countries-dropdown',
+  props: ['countries'],
+  methods: {
+  handleChange: function(){
+    console.log(this.selected);
+    eventBus.$emit('country-selected', this.selected)
+  }
+}
+}
+</script>
+
+<style lang="css" scoped>
+</style>
