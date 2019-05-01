6153a9c36b0c9169ccea9277c8e9290c54bc4fd8
Darren Shankland <ds@4pq.co.uk>
2019-05-01 17:25:12 +0100

    add mvp

---
+++ src/components/CountriesList.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div>
+    <p>The Countries List</p>
+    <ul>
+      <countries-list-item v-for="(country, index) in countries" :country="country" :key="index">{{country.name}}</countries-list-item>
+    </ul>
+  </div>
+
+</template>
+
+<script>
+import ListItem from './ListItem.vue';
+
+export default {
+  name: 'countries-list',
+  props: ['countries'],
+  components: {
+    "countries-list-item": ListItem
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
